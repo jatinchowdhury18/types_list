e73c2da47b4a72d2ec5c75de8a0fed9807176351
jatin <jatinchowdhury18@gmail.com>
2022-12-12 10:00:53 -0600

    Initial commit

---
+++ test/test.cpp
@@ -0,0 +1,77 @@
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+#include <string_view>
+#include <array>
+
+#include <types_list/types_list.hpp>
+
+struct Type0
+{
+    static constexpr std::string_view name = "type0";
+};
+
+struct Type1
+{
+    static constexpr std::string_view name = "type1";
+};
+
+struct Type2
+{
+    static constexpr std::string_view name = "type2";
+};
+
+TEST_CASE ("Types List Test")
+{
+    SECTION ("For Each In Type List Test")
+    {
+        using List = types_list::TypesList<
+            Type0,
+            Type1,
+            Type2>;
+
+        static constexpr auto typeNames = [] {
+            std::array<std::string_view, List::count> names {};
+            types_list::ForEach<List>::doForEach (
+                [&names] (auto index) {
+                    names[index] = List::AtIndex<index>::name;
+                });
+            return names;
+        }();
+
+        static_assert (typeNames[0] == "type0");
+        static_assert (typeNames[1] == "type1");
+        static_assert (typeNames[2] == "type2");
+    }
+
+    SECTION ("Contains Test")
+    {
+        static_assert (types_list::TypesList<std::string, int, float>::contains<int>);
+        static_assert (! types_list::TypesList<std::string, int, float>::contains<double>);
+    }
+
+    SECTION ("Insert Front Test")
+    {
+        using BaseList = types_list::TypesList<Type0>;
+        static_assert (std::is_same_v<types_list::TypesList<Type1, Type0>, BaseList::InsertFront<Type1>>);
+        static_assert (std::is_same_v<types_list::TypesList<Type1, Type2, Type0>, BaseList::InsertFront<Type1, Type2>>);
+    }
+
+    SECTION ("Insert Back Test")
+    {
+        using BaseList = types_list::TypesList<Type0>;
+        static_assert (std::is_same_v<types_list::TypesList<Type0, Type1>, BaseList::InsertBack<Type1>>);
+        static_assert (std::is_same_v<types_list::TypesList<Type0, Type1, Type2>, BaseList::InsertBack<Type1, Type2>>);
+    }
+
+    SECTION ("Concatenate Test")
+    {
+        static_assert (std::is_same_v<types_list::TypesList<int, float>, types_list::TypesList<int>::Concatenate<types_list::TypesList<float>>>);
+    }
+
+    SECTION ("Tuple Test")
+    {
+        static_assert (std::is_same_v<types_list::TypesList<int, float>, types_list::TupleList<std::tuple<int, float>>>);
+        static_assert (std::is_same_v<types_list::TypesList<int, float>::Types, std::tuple<int, float>>);
+    }
+}
+
